babc4466556c080e60fc7ddcc1285385b9b0d5ad
Mint <mintnguyen.pa@gmail.com>
2022-11-19 22:25:13 -0800

    feat: add weather api

--- App.tsx
+++ App.tsx
@@ -5,20 +5,32 @@ import { useEffect, useState } from "react";
 
 export default function App() {
 	const [errMsg, setErrMsg] = useState("");
+	const [currentWeather, setCurrentWeather] = useState();
 
 	const load = async () => {
 		try {
 			let { status } = await Location.requestBackgroundPermissionsAsync();
 
 			if (status !== "granted") {
-				setErrMsg("Access to the location is neede.");
+				setErrMsg("Access to the location is needed.");
 				return;
 			}
 
 			const location = await Location.getCurrentPositionAsync();
 
 			const { longitude, latitude } = location.coords;
-			alert(`Longitude: ${longitude}, Latitude: ${latitude}`);
+
+			const weatherUrl = `${process.env.BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&appid=${process.env.WEATHER_API_KEY}`;
+
+			const response = await fetch(weatherUrl);
+
+			const result = await response.json();
+
+			if (response.ok) {
+				setCurrentWeather(result);
+			} else {
+				setErrMsg(result.message);
+			}
 		} catch (err) {
 			console.log(err);
 		}
